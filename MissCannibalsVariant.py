99410a9f47cc90c06e9f56dcb66eb68e13b7d86b
Brandon <itzbrandonlee@yahoo.com>
2025-09-21 18:00:36 -0700

    Finished algorithm

--- MissCannibalsVariant.py
+++ MissCannibalsVariant.py
@@ -1,5 +1,7 @@
 from search import *
 
+CAPACITY = 3
+
 class MissCannibalsVariant(Problem):
 
     def __init__(self, N1=4, N2=4, goal=(0, 0, False)):
@@ -10,24 +12,68 @@ class MissCannibalsVariant(Problem):
         super().__init__(initial, goal)
     
     def result(self, state, action):
+        # applies action  strings, counts the amount of M or C and tracks whick side of the river it is on
         m_count = action.count('M')
         c_count = action.count('C')
-        boat_on_left = state[2]
+        onLeft = state[2]
         
-        if boat_on_left == True:
+        # moves from L > R 
+        if onLeft == True:
             m_left = state[0] - m_count
             c_left = state[1] - c_count
-            boat_on_left = False
-            new_state = (m_left, c_left, boat_on_left)
+            onLeft = False
+            new_state = (m_left, c_left, onLeft)
+        # moves from R > L
         else:
-            m_right = state[0] + m_count
-            c_right = state[1] + c_count
-            boat_on_left = True
-            new_state = (m_right, c_right, boat_on_left)
+            m_left = state[0] + m_count
+            c_left = state[1] + c_count
+            onLeft = True
+            new_state = (m_left, c_left, onLeft)
         return new_state
     
     def actions(self, state):
-        return state.actions
+        m_left, c_left, onLeft = state
+        m_right = self.N1 - m_left
+        c_right = self.N2 - c_left
+
+        # All posibilities of actions 
+        options_list = ('MCC', 'MMC', 'MMM', 'CCC', 'MC', 'CC', 'MM', 'M', 'C')
+        actions_list = []
+
+        for action in options_list: 
+            # safety check for capactity of boat 
+            if len(action) > CAPACITY:
+                continue
+
+            m_boat = action.count('M')
+            c_boat = action.count("C")
+            # makes sure missionaries cannot be outnumbered 
+            boat_ok = (m_boat == 0) or (m_boat >= c_boat)
+            if not boat_ok:
+                continue
+
+            # computs left and right bank counts based on action changes 
+            if onLeft:
+                if m_boat > m_left or c_boat > c_left:
+                    continue
+                new_m_left = m_left - m_boat
+                new_c_left = c_left - c_boat
+            else: 
+                if m_boat > m_right or c_boat > c_right:
+                    continue
+                new_m_left = m_left + m_boat
+                new_c_left = c_left + c_boat
+            
+            # makes sure the left and right bank are ok 
+            left_ok = (new_m_left == 0) or (new_m_left >= new_c_left)
+            m_right_new = self.N1 - new_m_left
+            c_right_new = self.N2 - new_c_left
+            right_ok = (m_right_new == 0) or (m_right_new >= c_right_new)
+
+            if left_ok and right_ok:
+                actions_list.append(action)
+
+        return actions_list
 
 if __name__ == '__main__':
     mc = MissCannibalsVariant(4,4)
@@ -37,9 +83,11 @@ if __name__ == '__main__':
     # print(mc.result((4,4,True), 'MMM'))
 
     # actions testing
+    # S1 = mc.actions((4,4,True))
+    # print(S1)
 
-    # path = depth_first_graph_search(mc).solution()
-    # print(path)
+    path = depth_first_graph_search(mc).solution()
+    print(path)
 
-    # path = breadth_first_graph_search(mc).solution()
-    # print(path) 
+    path = breadth_first_graph_search(mc).solution()
+    print(path) 
